68f0a88ca628e3be290de07a912f943f7580c5d5
Ubuntu <ubuntu@ip-172-31-11-144.ap-northeast-2.compute.internal>
2019-01-25 14:49:58 +0000

    kubernetes 스크립트 보완

---
+++ dockerfiles/mysql/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [ "$1" == "" ]; then
+  echo "Please. input vergion"
+  exit 1
+fi
+
+docker build -t 992189553983.dkr.ecr.ap-northeast-2.amazonaws.com/zimzua/db:$1 .
---
+++ dockerfiles/zimzua-api/start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/opt/zimzua-api/zimzua-api
